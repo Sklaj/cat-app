37612b5a5bb8029e00a79119971064c40252d2df
Sklaj <piotr.chodkowski@wp.pl>
2020-03-29 12:30:30 +0200

    added "add-pet" form markup & some styles

--- src/ts/components/AddPetView.tsx
+++ src/ts/components/AddPetView.tsx
@@ -1,20 +1,63 @@
 import * as React from "react";
-import {Link} from "react-router-dom";
 
 interface IProps {}
 
 export const AddPetView = (props: IProps) => {
+
+
+    const onSubmit = (e: any) => {
+        e.preventDefault();
+    };
+
     return (
-        <div>
+        <div className="add-pet">
             Dodaj zwierzaka
 
-            <Link to={`pet/1`}>
-                Pies
-            </Link>
+            <form className="add-pet-form">
+                <div className="radio-wrapper">
+                    <label htmlFor="pet-type-dog">Pies</label>
 
-            <Link to={`pet/098098`}>
-                Kot
-            </Link>
+                    <input type="radio" name="pet-type" id="pet-type-dog" value={1}/>
+
+                    <br/>
+
+                    <label htmlFor="pet-type-cat">Kot</label>
+
+                    <input type="radio" name="pet-type" id="pet-type-cat" value={2}/>
+                </div>
+
+                <div className="radio-wrapper">
+                    <label htmlFor="pet-sex-male">Male</label>
+
+                    <input type="radio" name="pet-sex" id="pet-sex-male"
+                           onChange={e => console.log("male", e.target.value)}
+                    />
+
+                    <br/>
+
+                    <label htmlFor="pet-sex-female">Female</label>
+
+                    <input type="radio" name="pet-sex" id="pet-sex-female"
+                           onChange={e => console.log("female", e.target.value)}
+                    />
+                </div>
+
+                <label htmlFor="pet-name">Name</label>
+
+                <input type="name" id="pet-name" name="name"/>
+
+                <label htmlFor="pet-age">Age</label>
+
+                <input type="age" id="pet-age" name="age"/>
+
+                <label htmlFor="pet-breed">Rasa</label>
+
+                <input type="breed" id="pet-breed" name="breed"/>
+
+                <button type="submit" name="save-pet" onClick={e => onSubmit(e)}>
+                    Zapisz zwierzaka
+                </button>
+            </form>
         </div>
     );
 };
